7eace7231c79a8ac4c0cf51016a2125b1b6cca71
José Guilherme <guilhermeverissimo14@gmail.com>
2023-09-25 10:23:33 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'detalhes', component:DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
